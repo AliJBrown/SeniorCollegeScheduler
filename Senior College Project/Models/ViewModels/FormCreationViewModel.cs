f8cc9197f9917ce99bd530016cb20870b1facd9e
justinridout <56090823+justinridout@users.noreply.github.com>
2020-02-07 11:37:11 -0600

    Made FormCreationViewModel

---
+++ Senior College Project/Models/ViewModels/FormCreationViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Senior_College_Project.Models.ViewModels
+{
+    public class FormCreationViewModel
+{
+        public String ProposedTitle { get; set; }
+        public int NumberOfSessions { get; set; }
+        public int LengthOfSession { get; set; }
+        public String CourseDescription { get; set; }
+        public String PreRequisite { get; set; }
+        //add Prefered Sessions times
+        public String UnavailableTimes { get; set; }
+        public int MinStudentCount { get; set; }
+        public int MaxStudentCount { get; set; }
+
+        [Range(0,100)]
+        public int ChairsNeeded { get; set; }
+        public int TablesNeeded { get; set; }
+        //add Equipment needed
+        public int OtherEquipmentNeeded { get; set; }
+        public double HandoutCost { get; set; }
+        
+        public String StipendRequested { get; set; }
+
+}
+}
