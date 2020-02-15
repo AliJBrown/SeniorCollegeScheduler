345df5724a16b79279e15df44958a66eb9fcc9b8
Alek Brown <ajbrown@dmacc.edu>
2020-02-15 01:36:34 -0600

    clean identity, test migrate (successful)

---
+++ IdentityExample/Controllers/ClassController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SeniorCollegeScheduler.Controllers
+{
+    public class ClassController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ProposedClassesOverview()
+        {
+            return View();
+        }
+
+        public IActionResult ProposedClassDetailed()
+        {
+            return View();
+        }
+    }
+}
---
+++ IdentityExample/Models/ViewModels/CreateClassCommand.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace SeniorCollegeScheduler.Models.ViewModels
+{
+    public class CreateClassCommand
+    {
+        //Still need preferred time variables
+        //NEED DATA ANOTATIONS
+        public DateTime TodaysDate { get; set; }
+        public string ProposedTitle { get; set; }
+        public string MailingAddress { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string ZipCode { get; set; }
+        public string LandPhone { get; set; }
+        public string CellPhone { get; set; }
+        public string EMail { get; set; }
+        public int NumClassSessions { get; set; }
+        public int LengthOfSession { get; set; }
+        public string ClassDesc { get; set; }
+        public string PreRequisites { get; set; }
+        public int MinStudentCount { get; set; }
+        public int MaxStudentCount { get; set; }
+        public int TablesNeeded { get; set; }
+        public int ChairsNeeded { get; set; }
+        public string OtherEquipmentNeeded { get; set; }
+        public double HandoutCost { get; set; }
+        public string StipendRequested { get; set; }
+    }
+}
+
