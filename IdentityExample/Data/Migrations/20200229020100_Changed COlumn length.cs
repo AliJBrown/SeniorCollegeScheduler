f735bf281f3cbcaa378b283356ed74c43b49e39a
justinridout <56090823+justinridout@users.noreply.github.com>
2020-02-28 21:49:08 -0600

    AddedInInstructorInformationFillingOut

---
+++ IdentityExample/Data/Migrations/20200229020100_Changed COlumn length.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SeniorCollegeScheduler.Data.Migrations
+{
+    public partial class ChangedCOlumnlength : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "InstructorId",
+                table: "User",
+                maxLength: 40,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "InstructorId",
+                table: "User",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 40,
+                oldNullable: true);
+        }
+    }
+}
---
+++ IdentityExample/Data/Migrations/20200229020633_Readded USERS.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SeniorCollegeScheduler.Data.Migrations
+{
+    public partial class ReaddedUSERS : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "User",
+                columns: table => new
+                {
+                    InstructorInfoId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    InstructorId = table.Column<string>(maxLength: 40, nullable: true),
+                    FirstName = table.Column<string>(maxLength: 25, nullable: true),
+                    LastName = table.Column<string>(maxLength: 25, nullable: true),
+                    StreetAddress = table.Column<string>(maxLength: 45, nullable: true),
+                    City = table.Column<string>(maxLength: 25, nullable: true),
+                    State = table.Column<string>(maxLength: 2, nullable: true),
+                    ZipCode = table.Column<string>(maxLength: 5, nullable: true),
+                    ShareEmail = table.Column<bool>(nullable: false),
+                    ShareMobilePhone = table.Column<bool>(nullable: false),
+                    ShareLandline = table.Column<bool>(nullable: false),
+                    LandlinePhone = table.Column<string>(maxLength: 10, nullable: true),
+                    MobilePhone = table.Column<string>(maxLength: 10, nullable: true),
+                    InstructorBio = table.Column<string>(nullable: true),
+                    IsFiled = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_User", x => x.InstructorInfoId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "User");
+        }
+    }
+}
